c8fff3f98f233811db6beea4afe545a1bb725a5d
Tom Draper <tomjdraper1@gmail.com>
2025-01-05 12:02:32 +0000

    feat: Replace missed ticks in recipe format

--- src/lib/format.js
+++ src/lib/format.js
@@ -3,5 +3,5 @@ export function formatRecipe(recipe) {
     if (recipe.startsWith('```')) {
         recipe = recipe.split('\n').slice(1, -1).join('\n');
     }
-    return recipe
+    return recipe.replaceAll('```', '');
 }
